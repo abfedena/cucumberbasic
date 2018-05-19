5c17ffbffb5ca4decad136fe0542a7ce692e8c39
abfedena <abfedena@users.noreply.github.com>
2018-05-19 10:17:55 +0530

    Update Hook.java

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -24,7 +24,7 @@ public class Hook extends BaseUtil{
 
         //Passing a dummy WebDriver instance step info
         base.StepInfo = "FirefoxDriver";
-        System.setProperty("webdriver.chrome.driver", "C:\chromedriver_win32\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
     }
 
